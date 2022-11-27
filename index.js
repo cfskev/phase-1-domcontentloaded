418bb956d33098c980941bfc30a590b585dee4e9
Kevin <kevin@crownfundingsource.com>
2022-11-26 19:07:45 -0500

    commit tree 11fb6d2c03a08fd47a7841856c4ac8401c773e1b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+function changeDOM(){
+    document.getElementById('text').textContent = "This is really cool!";
+}
+
+document.addEventListener('DOMContentLoaded', changeDOM);
